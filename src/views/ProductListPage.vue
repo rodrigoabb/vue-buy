e32ccf74db3dd2311562a13b4f1a16b8c64452c3
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-01-11 00:24:56 -0300

    Created base of Product List Page View

---
+++ src/views/ProductListPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="product-page">
+    <div class="">
+      Products!
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ProductListPage',
+};
+
+</script>
+
+<style lang="scss">
+
+/* ==========================================================================
+   Product List
+   ========================================================================== */
+
+.product-page {
+  /* height: 100%; */
+  height: fit-content;
+  min-height: 80vh;
+  /* min-height: 100%; */
+}
+
+
+</style>
