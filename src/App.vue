80eb53b00ac5bfe3e85cef97dd0e754fa80cff0c
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-01-10 23:08:12 -0300

    Added Base General CSS - Implemented Header component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,104 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <div> HOLA</div>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header/Header.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+/* ==========================================================================
+   Main Styles
+   ========================================================================== */
+
+html {
+    height: 100%;
+}
+
+body {
+  min-height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  font-family: 'Lato-Regular', sans-serif;
+  font-size: 13px;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;  /* Font smoothing tweaks for MacOSx in Gecko */
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #444A59;
+  background-color: #f8f8f8;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  box-sizing: border-box;
 }
+
+h1, p, figure {
+  margin: 0;
+  font-weight: normal;
+}
+
+a,
+a:hover,
+a:visited{
+  text-decoration: none;
+  color: #444A59;
+}
+
+ul, li {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+button {
+  appearance: none;
+  border: 0;
+}
+
+.button:focus {
+  outline: none;
+  -webkit-tap-highlight-color: transparent;
+}
+
+header,
+footer {
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+main {
+  display: flex;
+  flex-direction: row;
+  flex: 1 0 auto;
+  margin: 30px 0 20px;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+  // position: absolute;
+  max-width: 1240px;
+  padding: 30px 40px;
+  margin: 0 auto;
+}
+
+.error {
+  color: red;
+}
+
 </style>
