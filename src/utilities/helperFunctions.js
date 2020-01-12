f523f8047af60f4ff4c238ea822ee19c2f30e01c
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-01-12 17:54:37 -0300

    Documented helper functions file

--- src/utilities/helperFunctions.js
+++ src/utilities/helperFunctions.js
@@ -1,5 +1,25 @@
+/**
+ *  @module utilities/helperFunctions
+*/
+
+/**
+  * Takes number within a string and returns it casted to Number
+  * @param {String} string String to parse number from
+*/
 export const parseNumber = string => Number(string.replace(/[^0-9.]+/g, ''));
+
+/**
+  * Returns given string with its first letter capitalized
+  * @param {String} string String to capitalize first letter
+*/
 export const capitalizeFirstLetter = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+
+/**
+  * Returns given text, truncated at given max length, with ellipses at the end
+  * @param {String} text - String to truncate
+  * @param {Number} max - Max length for text to be truncated to
+*/
 export const truncateTextWithEllipses = (text, max) => {
   let shortString = '';
   if (text) {
