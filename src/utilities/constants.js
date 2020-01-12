d49693878d6da55632ec5270ce6d22ddc635a0d5
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-01-12 17:54:27 -0300

    Documented Constants file

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,6 +1,39 @@
+/**
+ * @module utilities/constants
+ *
+ */
+
+/**
+  * Defines an object with used currency information: 'code' and 'symbol'
+*/
 export const CURRENCY = { code: 'EUR', symbol: '€' };
+
+/**
+  * Defines the amount of producst per page to be shown on each page
+*/
 export const PRODUCTS_PER_PAGE = 6;
+
+/**
+  * Defines the amount wait time that fake loading spinner should be shown
+*/
 export const FAKE_WAIT_TIME_MS = 400;
+
+/**
+  * Defines the amount of pages available for fetching procuts
+*/
 export const PAGE_COUNT = 10;
+
+/**
+  * Defines an object with desired language for products information: 'name' and 'code'
+*/
+export const LANGUAGE = { name: 'italian', code: 'it' };
+
+/**
+  * Defines the max length of product descriptions on Product Page
+*/
 export const PRODUCT_DESCRIPTION_LIST_MAX_LENGTH = 100;
+
+/**
+  * Defines the max length of product titles on Cart Dropdown Component
+*/
 export const PRODUCT_TITLE_DROPDOWN_MAX_LENGTH = 60;
