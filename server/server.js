c8f226a735ad9e0843fd127fc35565907edb42d6
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-01-14 18:19:32 -0300

    Using process.env.PORT

--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ app.use(history({
 // 2nd call for redirected requests
 app.use(staticFileMiddleware);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
 });
